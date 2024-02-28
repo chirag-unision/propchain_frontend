e7577bb3316f5fbb7c2aa77a8d931d6b7f230f6b
chirag-unision <sharmacj123@gmail.com>
2024-02-29 02:39:03 +0530

    changes in eth provider

--- contexts/EthContext/EthProvider.jsx
+++ contexts/EthContext/EthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { useReducer, useCallback, useEffect } from "react";
 import Web3 from "web3";
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 import EthContext from "./EthContext";
 import { reducer, actions, initialState } from "./state";
 
