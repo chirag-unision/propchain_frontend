c6d93d6e98d16d6b60516b6d9e653b442b5a4249
Sourav <srvgarg332003@gmail.com>
2024-03-01 00:26:45 +0530

    added some things

--- src/components/common/topBar.jsx
+++ src/components/common/topBar.jsx
@@ -2,21 +2,33 @@
 
 const TopBar = ({ iconUrl = "/User.png", user_name = "Nakshatra Chandna", showAdd = true }) => {
     return (
-        <div className="w-full bg-[rgba(183,0,24,0.43)]  p-3 flex flex-row justify-between">
-            <div className=" flex flex-row gap-x-2">
+        <div className="w-full bg-[rgba(183,0,24,0.43)] px-8 py-4 flex flex-row justify-between">
+            <div className=" flex flex-row gap-x-6">
+
                 <div className="">
                     <img src={iconUrl} className="h-[2rem] w-[2rem] rounded-md "></img>
                 </div>
 
-                <div className="text-white text-center ">
+                <div className="text-white h-min mt-[0.25rem]">
                     {user_name}
+                </div>
+
+                <div className="text-white bg-[#1E2021] px-3 py-1 h-min rounded-lg ">
+                    Edit Profile 
                 </div>
 
             </div>
 
-            <span>
+            <div className="flex flex-row gap-x-6">
+                <div className="text-white bg-[#1E2021] px-3 py-1 h-min rounded-lg ">
+                    Add Another Property 
+                </div>
 
-            </span>
+                <div className="">
+                    <img src={"/Wallet.png"} className="h-[2rem] w-[2rem] rounded-md "></img>
+                </div>
+
+            </div>
 
         </div>
     );
--- src/pages/ListProperty.jsx
+++ src/pages/ListProperty.jsx
@@ -1,10 +1,43 @@
+import TopBar from "../components/common/topBar";
 
+
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
 
 
 const PropertyPage = () => {
     return (
         <div>
-        <h1>Property</h1>
+            <TopBar />
+            <div className="p-5 w-full">
+                <div className="flex flex-row justify-evenly rounded-2xl bg-[#181A1B]">
+                    <div className='w-1/5'>
+                        <Button
+                            className="w-full h-"
+                            component="label"
+                            role={undefined}
+                            variant="contained"
+                            tabIndex={-1}
+                            startIcon={<CloudUploadIcon />}
+                            >
+                            Upload file
+                            <VisuallyHiddenInput type="file" />
+                        </Button>
+                    </div>
+                    <div className='w-full'></div>
+                </div>
+
+            </div>
         </div>
     );
     }
