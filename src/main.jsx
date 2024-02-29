d3a76d233b909b14c3ebd88ae3534407b5ca94ae
chirag-unision <sharmacj123@gmail.com>
2024-02-29 22:05:53 +0530

    eth provider commented

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import EthProvider from "../contexts/EthContext/EthProvider";
+// import EthProvider from "../contexts/EthContext/EthProvider";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <EthProvider>
+    {/* <EthProvider> */}
       <App />
-    </EthProvider>
+    {/* </EthProvider> */}
   </React.StrictMode>,
 )
