819f9df3032523b805c09b34c6e290c727358c11
Sourav <srvgarg332003@gmail.com>
2024-02-29 21:57:18 +0530

    added structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter, Route, Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
