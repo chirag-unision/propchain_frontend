5ed5de7df63f1275f383e502dd1e333f9ce0f5b8
morpheus-30 <konnichiwasekai7@gmail.com>
2024-03-02 09:41:41 +0530

    _

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,9 +1,34 @@
 import { Button } from "@mui/material"
 import TopBar from "../components/common/topBar"
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../constants";
 
 
 
-const PropertyPage = () => {
+const PropertyPage = ({ pid = 1 }) => {
+
+    const [data, setData] = useState([]);
+
+    const getPropertyData = (pid) => {
+        axios.post(BASE_URL + "api/tenant/getpropinfo", {
+            pid: pid
+        }
+        ).then((res) => {
+            console.log(res.data);
+            setData(res.data);
+        }
+        ).catch((err) => {
+            console.log(err);
+        }
+        )
+    }
+    useEffect(() => {
+        getPropertyData(pid);
+    }
+        , [])
+
     return (
         <div>
             <TopBar />
@@ -12,7 +37,7 @@ const PropertyPage = () => {
                     <div className="flex flex-col p-10 w-[30vw]">
                         <img src="https://images.livemint.com/rf/Image-621x414/LiveMint/Period1/2014/12/16/Photos/house-ktQD--621x414@LiveMint.jpg" alt="property" className=" rounded-2xl " />
                         <div className="flex flex-col bg-nak-light-gray rounded-2xl mt-20">
-                            <div className="text-8xl text-white mx-auto mt-20">10K</div>
+                            <div className="text-8xl text-white mx-auto mt-20"></div>
                             <div className="text-2xl text-[#7B7B7B] mx-auto mb-20">per mo</div>
 
                         </div>
@@ -20,7 +45,7 @@ const PropertyPage = () => {
                     </div>
                     <div className="flex flex-col">
                         <div className="text-5xl text-white mt-10">Property Name</div>
-                        <div className="text-2xl text-white mt-10">2 BHK Builder Floor For Sale in Salasar Luxury Floors,Neharpar, Faridabad </div>
+                        <div className="text-2xl text-white mt-10">2 BHK Builder Floor For Sale in Salasar Luxury Floors,Neharpar, Faridab   </div>
                         <div className="flex flex-row bg-nak-light-gray w-100% h-[10vh] mr-10 mt-5 rounded-2xl">
                             <div className="text-white m-auto font-bold ">2 Beds</div>
                             <div className="border-r-2 h-[70%] m-auto border-white"></div>
